95eb5825125c1e36b6d060f421b8c8b49f50418c
Anastasiia Lozovan <138717408+stacylozovan@users.noreply.github.com>
2024-11-26 14:48:14 +0100

    Initial commitxcvs

---
+++ Sandbox/BasicGame/src/BasicGame.java
@@ -0,0 +1,37 @@
+import nl.saxion.app.SaxionApp;
+
+import nl.saxion.app.interaction.GameLoop;
+import nl.saxion.app.interaction.KeyboardEvent;
+import nl.saxion.app.interaction.MouseEvent;
+
+public class BasicGame implements GameLoop {
+
+    public static void main(String[] args) {
+        SaxionApp.startGameLoop(new BasicGame(), 1000, 1000, 40);
+    }
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() { //fhhdfhdhfdfdhtestetstetstetste
+    }
+
+    @Override
+    public void keyboardEvent(KeyboardEvent keyboardEvent) {
+
+    }
+
+    @Override
+    public void mouseEvent(MouseEvent mouseEvent) {
+
+    }
+}
+
+
+
+
+
+
