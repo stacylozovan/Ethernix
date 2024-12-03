5315885f277413e39705b5d8fdf307d4fc308100
Patrick Santos <patrick.sg.santos@gmail.com>
2024-12-03 12:10:41 +0100

    Add .gitignore and clean up tracked files

--- src/main/mainPlayer.java
+++
@@ -1,9 +0,0 @@
-public class mainPlayer {
-    int x;
-    int y;
-    String character = "Player"; // I named this string player, but ofc this is only to test later if the code works
-    // in the future the player will be the picture of the character
-    // I only created the necessary stuff for the character to move
-    //TODO for future stuff, someone may add levels, hearts etc... to this class :}
-}
-
--- src/object/OBJ_for_world.java
+++
@@ -1,2 +0,0 @@
-public class OBJ_for_world {
-}
