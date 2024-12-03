d1f94250a8b129a532e0938c329b00a866992354
naufal <naufalfidha2003@gmail.com>
2024-12-03 10:51:34 +0100

    added Tile and Map class

--- src/main/tile/Map.java
+++ src/main/tile/Map.java
@@ -1,29 +1,29 @@
 package tile;
 
-import java.awt.*;
-
 public class Map {
-    public Tile[][] tile;
+    private Tile[][] tile;
+
     public Map() {
-        int width=100;
-        int height=100;
-        tile = new Tile[16][16];
-        for (int i = 0; i < tile.length; i++) {
-            for (int j = 0; j < tile[i].length; j++) {
-                tile[i][j] = new Tile();
-                this.tile[i][j].image = "src/object/tiles/grass.png";
-                tile[i][j].x=width*i;
-                tile[i][j].y=height*j;
+        int tileSize = 32;
+        int rows = 64;
+        int cols = 64;
+
+        tile = new Tile[rows][cols];
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                tile[row][col] = new Tile();
+                tile[row][col].image = "src/object/tiles/wall.png";
+                tile[row][col].x = col * tileSize;
+                tile[row][col].y = row * tileSize;
             }
         }
     }
 
-    public void draw(){
-        for (int i = 0; i < tile.length; i++) {
-            for (int j = 0; j < tile[i].length; j++){
-                tile [i][j].drawTile();
+    public void draw() {
+        for (int row = 0; row < tile.length; row++) {
+            for (int col = 0; col < tile[row].length; col++) {
+                tile[row][col].drawTile();
             }
-
         }
     }
 }
--- src/main/tile/Tile.java
+++ src/main/tile/Tile.java
@@ -3,12 +3,10 @@ package tile;
 import nl.saxion.app.SaxionApp;
 
 public class Tile {
-    String image;
-    int x;
-    int y;
-    int width,height;
+    public String image;
+    public int x, y;
 
-    public void drawTile(){
-        SaxionApp.drawImage(image, x, y);
+    public void drawTile() {
+        SaxionApp.drawImage(image, x, y, 32, 32);
     }
 }
