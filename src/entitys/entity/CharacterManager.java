89485ee4a5fc98d2dd0f375a11bc87957aa1d663
Patrick Santos <patrick.sg.santos@gmail.com>
2024-12-13 10:27:53 +0100

    create the npc class, and add one npc in the screen

--- src/entitys/entity/CharacterManager.java
+++ src/entitys/entity/CharacterManager.java
@@ -1,51 +1,58 @@
 package entity;
 
 public class CharacterManager {
-    private final Player player;
-    private final Madara madara;
+
+    private Player player;
+    private Madara madara;
+    private NPC npc;
 
     public CharacterManager() {
+
         this.player = new Player();
         this.madara = new Madara();
+        this.npc = new NPC("mark", 500, 500, new String[]{"teste de dialogo", "teste dialogo 2"}, "down", "static");
 
         this.player.setDefaultValues();
         this.madara.setDefaultValues();
     }
 
+
     public void update(boolean[] keys) {
         player.update(keys);
-
+        madara.update(player);
     }
+
 
     public void handleCharacterInteractions() {
         if (Math.abs(player.getX() - madara.getX()) < 50 && Math.abs(player.getY() - madara.getY()) < 50) {
             player.takeDamage(10);
         }
 
-        // Handle player death
+
         if (player.getHealth() <= 0) {
             System.out.println("Player is dead!");
 
         }
 
-        // Handle Madara's death
         if (madara.getHealth() <= 0) {
-            System.out.println("Madara is defeated!");
+            System.out.println(" is defeated!");
 
         }
     }
 
-    public void draw(int playerScreenX, int playerScreenY, int cameraX, int cameraY) {
-        player.draw(playerScreenX, playerScreenY);
-        int madaraScreenX = madara.getX() - cameraX;
-        int madaraScreenY = madara.getY() - cameraY;
-        madara.draw(madaraScreenX, madaraScreenY);
+
+    public void draw() {
+        player.draw();
+        madara.draw();
+        npc.draw();
     }
+
 
     public void displayHealthStatus() {
         System.out.println("Player Health: " + player.getHealth());
-        System.out.println("Madara Health: " + madara.getHealth());
+        System.out.println(" Health: " + madara.getHealth());
     }
+
 
     public Player getPlayer() {
         return player;
@@ -53,17 +60,5 @@ public class CharacterManager {
 
     public Madara getMadara() {
         return madara;
-    }
-
-    // Check if the player is near Madara
-    public boolean isPlayerNearMadara() {
-        int playerX = player.getX();
-        int playerY = player.getY();
-
-        int madaraX = madara.getX();
-        int madaraY = madara.getY();
-
-        // Return true if the player is within a 50-pixel range of Madara's position
-        return Math.abs(playerX - madaraX) < 50 && Math.abs(playerY - madaraY) < 50;
     }
 }
